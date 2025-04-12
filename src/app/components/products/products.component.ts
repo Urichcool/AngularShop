6e45b81df8765efdf2dfbc162ba8dc4029e53fc3
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2025-04-12 21:41:15 +0200

    implemented-products-service

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import {MatCardImage, MatCardModule} from '@angular/material/card';
+import { ProductsService } from '../../services/products.service';
+import { IProduct } from '../../interfaces/products.interface';
 
 @Component({
   selector: 'app-products',
@@ -8,6 +10,15 @@ import {MatCardImage, MatCardModule} from '@angular/material/card';
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
+  products!: IProduct[]
+productsService = inject(ProductsService);
 
+
+ngOnInit(): void {
+  this.productsService.getProducts().subscribe((data) => {
+    this.products = data
+    console.log(this.products)
+  }).unsubscribe()
+}
 }
--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IProduct } from '../interfaces/products.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductsService {
+  url: string = 'http://localhost:3000/products';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getProducts() {
+   return this.http.get<IProduct[]>(this.url);
+  }
+
+  getProduct(id: number) {
+    return this.http.get<IProduct>(`${this.url}/${id}`);
+  }
 }
