76cefcef3cd34bd08c14fe84cf79c653a7c4df97
Yurii Novikov <104870932+Urichcool@users.noreply.github.com>
2025-04-15 22:45:30 +0200

    created-product-details-page

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from '../../interfaces/products.interface';
+import { MatCardModule } from '@angular/material/card';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [MatCardModule, CurrencyPipe],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+product!:IProduct
+route = inject(ActivatedRoute);
 
+
+ngOnInit(){ 
+  this.route.data.subscribe((data) =>
+    this.product = data["data"]
+  )
+ 
+  
+}
 }
